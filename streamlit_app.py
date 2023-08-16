e6cd957ed4f9696b317e7b15b54450cb04b64cea
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-08-16 06:32:35 +0000

    Working spike

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
+from typing import Any, Callable
+
 import faiss
 import streamlit as st
 from langchain.agents import Tool
 from langchain.chat_models import ChatOpenAI
 from langchain.docstore import InMemoryDocstore
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ChatMessageHistory
+from langchain.schema.messages import BaseMessage
 from langchain.tools.file_management.read import ReadFileTool
 from langchain.tools.file_management.write import WriteFileTool
 from langchain.utilities import GoogleSearchAPIWrapper
 from langchain.vectorstores import FAISS
 from langchain_experimental.autonomous_agents import AutoGPT
+from pydantic import BaseModel
 
-st.sidebar.text_input("OpenAI API Key", key="api_key")
+st.sidebar.text_input("OpenAI API Key (not used yet, use secrets.toml)", key="api_key")
 
 st.title("Researcher Agent")
 
@@ -33,7 +38,7 @@ tools = [
 
 
 # Set up the memory
-@st.cache
+@st.cache_resource
 def vector_store():
     embeddings_model = OpenAIEmbeddings(model="text-embedding-ada-002")
     embedding_size = 1536
@@ -42,11 +47,18 @@ def vector_store():
     return FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
 
 
-# Set up the model and AutoGPT
+class ChatMessageHistoryWithHook(ChatMessageHistory):
+    hook: Callable[[str], Any]
+
+    def add_message(self, message: BaseMessage) -> None:
+        """Add a self-created message to the store"""
+        super().add_message(message)
+        self.hook(message.content)
 
 
 if user_query:
     vectorstore = vector_store()
+    history = ChatMessageHistoryWithHook(hook=st.chat_message("AI").write)
 
     agent = AutoGPT.from_llm_and_tools(
         ai_name="Jimbo",
@@ -54,8 +66,9 @@ if user_query:
         tools=tools,
         llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
         memory=vectorstore.as_retriever(),
+        chat_history_memory=history,
     )
     # Set verbose to be true
     agent.chain.verbose = True
 
-    agent.run([user_query])
+    result = agent.run([user_query])
